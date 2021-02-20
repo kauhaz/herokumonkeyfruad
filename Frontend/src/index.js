c425c6a556c1e3d23ba1f0362a90eed18eb2db42
huak <komsark_@hotmail.co.th>
2021-02-21 03:13:31 +0700

    commit

--- Frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
-ReactDOM.render(<App />, document.getElementById("root"));
